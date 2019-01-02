bde91b3f3a3fc8938505ce498c90ccb60838f8a7
qjb1991 <375512114@qq.com>
2019-01-03 00:57:23 +0800

    0103 curl

--- app/portal/model/TouristMsg.php
+++ app/portal/model/TouristMsg.php
@@ -22,8 +22,9 @@ class TouristMsg extends Model
         if ($result) {
             Db::table('lg_tourist_msg')->where('ip', $ip)->setInc('login_sum');
         } else {
-            $url = 'http://ip.taobao.com/service/getIpInfo.php?ip=61.140.74.44';
-            $ipMsg = file_get_contents($url, false); 
+            $url = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$ip;
+            // $ipMsg = file_get_contents($url, false); 
+            $ipMsg = $this->http_get($url);
             $data = [
                 'ip' => $ip,
                 'create_at' => time(),
@@ -31,10 +32,30 @@ class TouristMsg extends Model
             ];
             if (!empty($ipMsg)) {
                 $ipMsg = json_decode($ipMsg, true);
-                $data['city'] = $ipMsg['data']['country'].$ipMsg['data']['region'].$ipMsg['data']['city'];
+                if ($ipMsg['code'] === 0) {
+                    $data['city'] = $ipMsg['data']['country'].$ipMsg['data']['region'].$ipMsg['data']['city'];
+                }
             }
             $re = Db::table('lg_tourist_msg')->insert($data);
         }
 
+    }
+
+    public function http_get($url)
+    {
+        //初始化
+        $curl = curl_init();
+        //设置抓取的url
+        curl_setopt($curl, CURLOPT_URL, $url);
+        //设置头文件的信息作为数据流输出 (关闭)
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        //设置获取的信息以文件流的形式返回，而不是直接输出。
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        //执行命令
+        $data = curl_exec($curl);
+        //关闭URL请求
+        curl_close($curl);
+        //显示获得的数据
+        return $data;
     }
 }
