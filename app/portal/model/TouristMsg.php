697a4fad82781f06e1311e88c53ba20dd87b0f99
qjb1991 <375512114@qq.com>
2019-01-03 00:31:56 +0800

    0103 add city

--- app/portal/model/TouristMsg.php
+++ app/portal/model/TouristMsg.php
@@ -17,15 +17,22 @@ class TouristMsg extends Model
             'create_at' => ['between', [$beginToday, $endToday]]
         ];
         $result = Db::table('lg_tourist_msg')
-            ->where($condition)
-            ->find();
+        ->where($condition)
+        ->find();
         if ($result) {
             Db::table('lg_tourist_msg')->where('ip', $ip)->setInc('login_sum');
         } else {
+            $url = 'http://ip.taobao.com/service/getIpInfo.php?ip=61.140.74.44';
+            $ipMsg = file_get_contents($url, false); 
             $data = [
                 'ip' => $ip,
                 'create_at' => time(),
+                'login_sum' => 1
             ];
+            if (!empty($ipMsg)) {
+                $ipMsg = json_decode($ipMsg, true);
+                $data['city'] = $ipMsg['data']['country'].$ipMsg['data']['region'].$ipMsg['data']['city'];
+            }
             $re = Db::table('lg_tourist_msg')->insert($data);
         }
 
